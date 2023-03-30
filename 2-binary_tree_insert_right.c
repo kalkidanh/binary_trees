44e78473d2017b5e3744067aa3abfce826d0ce2b
kalkidanh <kalkidanh89@gmail.com>
2023-03-30 10:49:23 -0700

    is leaf

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,22 +11,20 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = parent->right;
-
 	if (parent->right != NULL)
-		parent->right = new_node;
-
-	return (new_node);
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
 }
