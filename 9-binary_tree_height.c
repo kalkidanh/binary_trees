c2cb69dc575436b6cb32430b476c0033a527da99
kalkidanh <kalkidanh89@gmail.com>
2023-03-30 11:57:53 -0700

    init vars to 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		size_t lh = 0, rh = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+		lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((lh > rh) ? lh : rh);
+	}
+	return (0);
 }
